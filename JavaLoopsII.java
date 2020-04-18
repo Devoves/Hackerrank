73587763e0ac863b42309b166979e2ee9d65818b
Devoves <63800077+Devoves@users.noreply.github.com>
2020-04-19 00:29:42 +0200

    solution

--- JavaLoopsII.java
+++ JavaLoopsII.java
@@ -2,15 +2,25 @@ import java.util.*;
 import java.io.*;
 
 class Solution{
-    public static void main(String []argh){
+    public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        int t=in.nextInt();
-        for(int i=0;i<t;i++){
-            int a = in.nextInt();
-            int b = in.nextInt();
-            int n = in.nextInt();
+        int t = in.nextInt();
+        if (0 <= t && t <= 500) {
+            for (int i = 0; i < t; i++) {
+                int a = in.nextInt();
+                int b = in.nextInt();
+                int n = in.nextInt();
+                if (0 <= a && a <= 50 && 0 <= b && b <= 50 && 1 <= n && n <= 15) {
+                    for (int j = 0, add = 0; j < n; j++) {
+                        add = (int) (Math.pow(2, j) * b + add);
+                        int result = a + add;
+                        System.out.print(result + " ");
+                    }
+                    System.out.println("");
+                }
+            }
+            in.close();
         }
-        in.close();
     }
 }
 
