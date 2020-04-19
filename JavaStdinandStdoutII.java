0fe78b66b2befd099152aa258fc5bf9b5ed30b83
Devoves <63800077+Devoves@users.noreply.github.com>
2020-04-20 00:28:08 +0200

    Solution

--- JavaStdinandStdoutII.java
+++ JavaStdinandStdoutII.java
@@ -5,8 +5,8 @@ public class Solution {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
-
-        // Write your code here.
+        double d = scan.nextDouble();
+        String s = scan.nextLine() + scan.nextLine();
 
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
