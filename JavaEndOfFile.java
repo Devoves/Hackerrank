270aada9c69d14741fd71f26b9e1df172ba6b956
Devoves <63800077+Devoves@users.noreply.github.com>
2020-04-21 14:43:19 +0200

    Create JavaEndOfFile.java

---
+++ JavaEndOfFile.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class JavaEndOfFile {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+    }
+}
+
+/*
+https://www.hackerrank.com/challenges/java-end-of-file/problem
+*/
