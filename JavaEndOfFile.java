23223a6b8b2922667d746edbf9d513d6fe890f9e
Devoves <63800077+Devoves@users.noreply.github.com>
2020-04-21 15:21:22 +0200

    Solution

--- JavaEndOfFile.java
+++ JavaEndOfFile.java
@@ -7,7 +7,11 @@ import java.util.regex.*;
 public class JavaEndOfFile {
 
     public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+        Scanner sc = new Scanner(System.in);
+        for(int i = 1; sc.hasNext(); i++){
+            System.out.print(i + " ");
+            System.out.println(sc.nextLine());
+        }
     }
 }
 
