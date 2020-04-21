3c9028775faf89f0c90f43cd4411ae4484f8e966
Devoves <63800077+Devoves@users.noreply.github.com>
2020-04-21 14:16:21 +0200

    Solution

--- JavaDatatypes.java
+++ JavaDatatypes.java
@@ -20,9 +20,9 @@ class JavaDatatypes {
                 long x=sc.nextLong();
                 System.out.println(x+" can be fitted in:");
                 if(x>=-128 && x<=127)System.out.println("* byte");
-                if(x>=-32768 && x<=32767)System.out.println("* short");
-                if(x>=-2147483648 && x<=2147483647)System.out.println("* int");
-                if(x>=-9223372036854775807L && x<=9223372036854775806L)System.out.println("* long");
+                if(x>=-Math.pow(2, 15) && x<=Math.pow(2, 15) - 1)System.out.println("* short");
+                if(x>=-Math.pow(2, 31) && x<=Math.pow(2, 31) - 1)System.out.println("* int");
+                if(x>=-Math.pow(2, 63) && x<=Math.pow(2, 63) - 1)System.out.println("* long");
             }
             catch(Exception e)
             {
