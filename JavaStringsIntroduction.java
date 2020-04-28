7df76b1285c54ccf2173ded44848882d929f6d88
Devoves <63800077+Devoves@users.noreply.github.com>
2020-04-28 16:13:48 +0200

    Solution

--- JavaStringsIntroduction.java
+++ JavaStringsIntroduction.java
@@ -1,15 +1,22 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class JavaStringsIntroduction {
 
     public static void main(String[] args) {
-        
         Scanner sc=new Scanner(System.in);
         String A=sc.next();
         String B=sc.next();
         /* Enter your code here. Print output to STDOUT. */
-        
+        int aBLength = A.length() + B.length();
+        System.out.println(aBLength);
+        int compare = A.compareTo(B);
+        if (compare > 0){
+            System.out.println("Yes");
+        } else {
+        System.out.println("No");
+        }
+        System.out.print(A.substring(0,1).toUpperCase() + A.substring(1) + " " + B.substring(0,1).toUpperCase() + B.substring(1));
     }
 }
 
