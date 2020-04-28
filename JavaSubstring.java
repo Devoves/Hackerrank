fb19b6147da2e89d9a97560bb5776c06c04da05b
Devoves <63800077+Devoves@users.noreply.github.com>
2020-04-28 17:36:22 +0200

    Solution

--- JavaSubstring.java
+++ JavaSubstring.java
@@ -11,5 +11,14 @@ public class JavaSubstring {
         String S = in.next();
         int start = in.nextInt();
         int end = in.nextInt();
+        System.out.println(S.substring(start, end));
     }
 }
+
+/*
+https://www.hackerrank.com/challenges/java-substring/problem?h_r=next-challenge&h_v=zen
+Practice
+Java
+Strings
+Java Strings Introduction
+*/
