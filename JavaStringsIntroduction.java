ad79f50f6a042b3a84747f97608551403e722cf5
Devoves <63800077+Devoves@users.noreply.github.com>
2020-04-28 16:51:23 +0200

    Solution

--- JavaStringsIntroduction.java
+++ JavaStringsIntroduction.java
@@ -8,14 +8,22 @@ public class JavaStringsIntroduction {
         String A=sc.next();
         String B=sc.next();
         /* Enter your code here. Print output to STDOUT. */
-        int aBLength = A.length() + B.length();
-        System.out.println(aBLength);
+        System.out.println(A.length() + B.length());
+        /*
+        As an alternative solution:
+        System.out.println((A+B).length());
+        */
         int compare = A.compareTo(B);
         if (compare > 0){
             System.out.println("Yes");
         } else {
         System.out.println("No");
         }
+        /*
+        As an alternative solution:
+        System.out.println(A.compareTo(B)>0?"Yes":"No");
+        */
+        
         System.out.print(A.substring(0,1).toUpperCase() + A.substring(1) + " " + B.substring(0,1).toUpperCase() + B.substring(1));
     }
 }
