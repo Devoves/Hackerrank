9b1fe5a09842fda5370b684bacfca6c32731a4fc
Devoves <63800077+Devoves@users.noreply.github.com>
2020-05-02 22:36:16 +0200

    Solution

--- JavaStringReverse.java
+++ JavaStringReverse.java
@@ -1,14 +1,32 @@
 import java.io.*;
 import java.util.*;
 
-public class Solution {
+public class JavaStringReverse {
+    
+	/**
+	 * Given a string A, print Yes if it is a palindrome (the direction of reading
+	 * the word do not change the meaning of it), print No otherwise.
+	 */
 
     public static void main(String[] args) {
-        
-        Scanner sc=new Scanner(System.in);
-        String A=sc.next();
-        /* Enter your code here. Print output to STDOUT. */
-        
+		Scanner sc = new Scanner(System.in);
+		String A = sc.next();
+		sc.close();
+		boolean same = true;
+
+		for (int i = 0; i < A.length() / 2; i++) {
+			if (A.substring(i, i + 1).equals(A.substring(A.length() - i - 1, A.length() - i))) {
+				same = true;
+			} else {
+				same = false;
+				System.out.println("No");	
+				break;
+			}
+		}
+		
+		if (same) {
+			System.out.println("Yes");
+		}
     }
 }
 
