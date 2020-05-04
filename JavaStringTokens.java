9c62bbf085cbd4c3c26df9da896914d73def4f38
Devoves <63800077+Devoves@users.noreply.github.com>
2020-05-04 22:28:59 +0200

    Create JavaStringTokens.java

---
+++ JavaStringTokens.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class JavaStringTokens {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String s = scan.nextLine();
+        // Write your code here.
+        scan.close();
+    }
+}
+
+/*
+https://www.hackerrank.com/challenges/java-string-tokens/problem
+*/
