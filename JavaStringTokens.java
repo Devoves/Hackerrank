980a8f2b916ef8dbcaeeda857197f64553605a1b
Devoves <63800077+Devoves@users.noreply.github.com>
2020-05-04 22:29:47 +0200

    Solution

--- JavaStringTokens.java
+++ JavaStringTokens.java
@@ -4,10 +4,19 @@ import java.util.*;
 public class JavaStringTokens {
 
     public static void main(String[] args) {
-        Scanner scan = new Scanner(System.in);
-        String s = scan.nextLine();
-        // Write your code here.
-        scan.close();
+		Scanner scan = new Scanner(System.in);
+		String s = scan.nextLine();
+		if (s == null || s.trim().equals("")) {
+			System.out.println("0");
+		} else {
+			String[] parts = s.trim().split("[^a-zA-Z0-9]+");
+			System.out.println(parts.length);
+			for (int i = 0; i < parts.length; i++) {
+				String word = parts[i];
+				System.out.println(word);
+			}
+		}
+		scan.close();
     }
 }
 
