464f9def0e261f30e51cbe87cee604552c8de36c
Devoves <63800077+Devoves@users.noreply.github.com>
2020-05-04 17:21:35 +0200

    Create PatternSyntaxChecker.java

---
+++ PatternSyntaxChecker.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class PatternSyntaxChecker{
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		int testCases = Integer.parseInt(in.nextLine());
+		while(testCases>0){
+			String pattern = in.nextLine();
+          	//Write your code
+		}
+	}
+}
+
+/*
+https://www.hackerrank.com/challenges/pattern-syntax-checker/problem
+*/
