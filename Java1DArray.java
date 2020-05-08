2cff84757f730799140b02fac42147145c6d71da
Andrii Velychko <63800077+Devoves@users.noreply.github.com>
2020-05-08 22:59:32 +0200

    Create Java1DArray.java

---
+++ Java1DArray.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Java1DArray {
+
+    public static void main(String[] args) {
+	   
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+
+        scan.close();
+
+        // Prints each sequential element in array a
+        for (int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
+    }
+}
+
+/*
+https://www.hackerrank.com/challenges/java-1d-array-introduction/problem
+*/
